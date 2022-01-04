41a1c3e50c6f209c5be92ec7f7fb88a52a88cc89
gouz7514 <gouz7514@gmail.com>
2022-01-05 00:24:12 +0900

    Update : props 사용

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      title : 'Climbing Academy',
+      amount : '294.67',
+      date : new Date(2022, 0, 1).toLocaleDateString(),
+    },
+    {
+      title : 'Climbing Academy',
+      amount : '294.67',
+      date : new Date(2022, 0, 1).toLocaleDateString(),
+    },
+    {
+      title : 'Climbing Academy',
+      amount : '294.67',
+      date : new Date(2022, 0, 1).toLocaleDateString(),
+    },
+  ]
+
   return (
     <div>
       <h1>
         This is React app
       </h1>
-      <ExpenseItem />
+      {
+        expenses.map((expense, idx) => {
+          return (
+            <ExpenseItem
+              key={ idx }
+              title={ expense.title }
+              amount={ expense.amount }
+              date={ expense.date }
+            />
+          )
+        })
+      }
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,16 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 0, 1).toLocaleDateString()
-  const expenseTitle = 'Climbing academy'
-  const expenseAmount = 294.67
+function ExpenseItem(props) {
 
   return (
     <div className='expense-item'>
-      <div>{ expenseDate }</div>
+      <div>{ props.date }</div>
       <div className='expense-item__description'>
         <h2>
-          { expenseTitle }
+          { props.title }
         </h2>
         <div className='expense-item__price'>
-          { expenseAmount }
+          { props.amount }
         </div>
       </div>
     </div>
