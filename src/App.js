a5c6609f4a2bf8d3e7954b08ae23583404842da2
gouz7514 <gouz7514@gmail.com>
2022-01-05 00:35:25 +0900

    Update : 컴포넌트에서 자바스크립트로 prop 갖고놀기

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ function App() {
   const expenses = [
     {
       title : 'Climbing Academy',
-      amount : '294.67',
+      amount : 294.67,
       date : new Date(2022, 0, 1).toLocaleDateString(),
     },
     {
-      title : 'Climbing Academy',
-      amount : '294.67',
-      date : new Date(2022, 0, 1).toLocaleDateString(),
+      title : 'Birthday Party',
+      amount : 100.00,
+      date : new Date(2022, 0, 13).toLocaleDateString(),
     },
     {
-      title : 'Climbing Academy',
-      amount : '294.67',
-      date : new Date(2022, 0, 1).toLocaleDateString(),
+      title : 'Play With Friends',
+      amount : 32.5,
+      date : new Date(2022, 0, 15).toLocaleDateString(),
     },
   ]
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+  console.log(props.date)
+  const year = props.date.split('.')[0]
+  const month = props.date.split('.')[1]
+  const day = props.date.split('.')[2]
 
   return (
     <div className='expense-item'>
-      <div>{ props.date }</div>
+      <div>
+        <div>{ year }</div>
+        <div>{ month }</div>
+        <div>{ day }</div>
+      </div>
       <div className='expense-item__description'>
         <h2>
           { props.title }
