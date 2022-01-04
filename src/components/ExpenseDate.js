fdc5d1826a3acd05d28f4de58ed77c662f6d9c6b
gouz7514 <gouz7514@gmail.com>
2022-01-05 00:42:22 +0900

    Update : 컴포넌트 잘게 쪼개기

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import '../style/ExpenseDate.css'
+
+function ExpenseDate(props) {
+  const year = props.date.split('.')[0]
+  const month = props.date.split('.')[1]
+  const day = props.date.split('.')[2]
+
+  return (
+    <div className='expense-date'>
+      <div className='expense-date__year'>{ year }</div>
+      <div className='expense-date__month'>{ month }</div>
+      <div className='expense-date__day'>{ day }</div>
+    </div>
+  )
+}
+
+export default ExpenseDate
