2468b7ee8ee0d0e6052afc07a1a9a6391ab52f8a
gouz7514 <gouz7514@gmail.com>
2022-01-05 00:11:23 +0900

    Update : 컴포넌트에서 동적 데이터 사용

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,20 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 0, 1).toLocaleDateString()
+  const expenseTitle = 'Climbing academy'
+  const expenseAmount = 294.67
+
   return (
     <div className='expense-item'>
-      <div>Date</div>
+      <div>{ expenseDate }</div>
       <div className='expense-item__description'>
         <h2>
-          Title
+          { expenseTitle }
         </h2>
-        <div className='expense-item__price'>Amount</div>
+        <div className='expense-item__price'>
+          { expenseAmount }
+        </div>
       </div>
     </div>
   )
