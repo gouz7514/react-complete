357992d8433d3c1c327f03aacb0392d86f016b62
gouz7514 <gouz7514@gmail.com>
2022-01-05 00:02:09 +0900

    Update : ExpenseItem 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <h1>
         This is React app
       </h1>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>
+          Title
+        </h2>
+        <div>Amount</div>
+      </div>
+    </div>
+  )
+}
+
+export default ExpenseItem
