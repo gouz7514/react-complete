075c73c29c7dbd40ddfbbf93b313b2d6ecdf0221
gouz7514 <gouz7514@gmail.com>
2022-01-06 00:05:01 +0900

    "Update : State를 다루는 방법"
    한번에 관리하는 경우 잘못된 상태에 대한 의존 발생 가능
    이전 상태에 기반한다면 prevState를 useState의 두번째 파라미터로 넘겨서 사용 가능

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,16 +5,41 @@ const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState('')
   const [enteredAmount, setEnteredAmount] = useState('')
   const [enteredDate, setEnteredDate] = useState('')
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: ''
+  // })
 
   const titleChangeHandler = (event) => {
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value
+    // })
+
+    // 이전 상태에 기반한다면 아래와 같이
+    // setUserInput((prevState) => {
+    //   return {
+    //     ...prevState,
+    //     enteredTitle: event.target.value
+    //   }
+    // })
     setEnteredTitle(event.target.value)
   }
 
   const amountChangeHandler = (event) => {
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value
+    // })
     setEnteredAmount(event.target.value)
   }
   
   const dateChangeHandler = (event) => {
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value
+    // })
     setEnteredDate(event.target.value)
   }
 
