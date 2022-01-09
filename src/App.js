8bb3544fd68bd1b0518fc894891842a3505fa2a4
gouz7514 <gouz7514@gmail.com>
2022-01-09 18:10:38 +0900

    Update : Keys 이해하기

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import Expenses from './components/Expenses/Expenses';
 
 const DUMMY_EXPENSES = [
   {
+    id: '1',
     title : 'Climbing Academy',
     amount : 294.67,
     date : new Date(2022, 1, 1).toLocaleDateString(),
   },
   {
+    id: '2',
     title : 'Birthday Party',
     amount : 100.00,
     date : new Date(2022, 1, 13).toLocaleDateString(),
   },
   {
+    id: '3',
     title : 'Play With Friends',
     amount : 32.5,
     date : new Date(2022, 1, 15).toLocaleDateString(),
